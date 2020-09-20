20efd71b36b5e11c9c40d20d6dfe14cd5cfd415e
jlenonns@gmail.com <jlenonns@gmail.com>
2020-09-19 21:20:51 -0300

    ajax

--- marvelApiTest/main.js
+++ marvelApiTest/main.js
@@ -35,17 +35,27 @@ $(document).ajaxComplete(function(ev) {
     console.log('elem: ', elem);
     console.log('idx: ', idx);
     
-    let markupDefined = '<br /><img src=\"';
+    let markupDefined = '<br /><img class=\"thumbnail\" src=\"';
     console.log('thumb', markupDefined);
     
     if(elem.thumbnail) {
-      markupDefined = markupDefined + elem.thumbnail + '\" />';
+      console.log('elem.thunb **********',  elem.thumbnail.path + '.' + elem.thumbnail.extension);
+      
+      markupDefined = markupDefined + elem.thumbnail.path + '.' + elem.thumbnail.extension + '\" />';
     } else {
       markupDefined = markupDefined + '\" />';
     }
     
     if(elem.description) {
-      markupDefined = markupDefined + '<p>' + elem.description + '</p>';
+      markupDefined = markupDefined + '<p class=\"description\">' + elem.description + '</p>';
+    }
+    
+    if(elem.title) {
+      markupDefined = markupDefined + '<p class=\"title\">' + elem.title + '</p';
+    }
+    
+    if(elem.variantDescription) {
+      markupDefined = markupDefined + '<p class=\"variantDescription\">' + elem.variantDescription + '</p>';
     }
     
     $('.js-marvelCardsBody').html($('.js-marvelCardsBody').html() + markupDefined);
